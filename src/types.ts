8c09c8320fa4c022b07250358ba0b5921fa9689c
playerkousas-rgb <playerkousas@gmail.com>
2026-05-07 08:49:21 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,5 @@
 export interface Bird {
   id: string;
   name: string;
-  scientificName: string;
-  category: string;
-  height: string;
-  weight: string;
-  description: string;
   imageUrl: string;
-  habitat: string;
-  diet: string;
-  rarity: 'Common' | 'Uncommon' | 'Rare' | 'Legendary';
-  discovered: boolean;
 }
