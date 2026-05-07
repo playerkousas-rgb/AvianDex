f7f0960ce4b4362b6b2f546dfc63d7c09e67f18f
playerkousas-rgb <playerkousas@gmail.com>
2026-05-07 08:50:39 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,14 @@ export interface Bird {
   id: string;
   name: string;
   imageUrl: string;
+  // 以下全部加個問號，代表「選填」，就不會報錯了
+  scientificName?: string;
+  category?: string;
+  height?: string;
+  weight?: string;
+  description?: string;
+  habitat?: string;
+  diet?: string;
+  rarity?: string;
+  discovered?: boolean;
 }
