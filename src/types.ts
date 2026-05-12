63dd689f1b3b86e2c387a598e5d5b06be34f717c
playerkousas-rgb <playerkousas@gmail.com>
2026-05-12 13:41:58 +0800

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,7 @@
-export interface Bird {
+eexport interface Bird {
   id: string;
   name: string;
+  englishName?: string; // 👈 這裡一定要有，不然上面那段還是會報錯
   imageUrl: string;
   // 以下全部加個問號，代表「選填」，就不會報錯了
   scientificName?: string;
